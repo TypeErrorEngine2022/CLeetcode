7ad651bdd6f5740c3a4ecafc2ba3c896e0b65e16
TypeErrorEngine2022 <jackychen924@gmail.com>
2023-03-17 13:30:23 +0800

    heapift and heapSort

--- LC378_KthSmallestInSortedMatrix.c
+++ LC378_KthSmallestInSortedMatrix.c
@@ -2,5 +2,19 @@
 #include <stdio.h>
 
 int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int k){
-    Heap* heap = createH 
+    Heap* heap = createMinHeap(k);
+
+    for (int i = 0; i < matrixSize; i++)
+        for (int j = 0; j < matrixSize; j++)
+        insert(heap, matrix[i][j]);
+
+    int res = -1;
+    for (int i = 0; i < k; i++) {
+        res = delMax(heap);
+    }
+    return res;
+}
+
+int main() {
+    return 0;
 }
