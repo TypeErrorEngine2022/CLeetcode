90c48138239aeee893bee7b76bf646c6202711ed
TypeErrorEngine2022 <jackychen924@gmail.com>
2023-03-27 01:27:24 +0800

    add sample code

--- LC347_TopKFrequentElements.c
+++ LC347_TopKFrequentElements.c
@@ -38,10 +38,66 @@ int* topKFrequent(int* nums, int numsSize, int k, int* returnSize){
     return res; 
 }
 
+
+
+//// Sample 
+typedef struct
+{
+    int num;
+    int count;
+}Dict;
+
+static int CompareDict(const void* x, const void * y)
+{
+    return ((Dict*)y)->count - ((Dict *)x)->count;
+}
+    
+
+int* topKFrequentSample(int* nums, int numsSize, int k, int* returnSize){
+
+    Dict *dict;
+    int hash[20001] = {0};
+    
+    int c = 0;
+    
+    for(int i =0;i<numsSize;i++)
+    {
+        hash[nums[i]+10000]++;
+        
+    }
+    
+    dict = (Dict *) malloc(sizeof(Dict)*numsSize);
+    
+    memset(dict,0,sizeof(Dict)*numsSize);
+    
+    for(int i=0;i<20001;i++)
+    {
+        if(hash[i]>0)
+        {
+            dict[c].num = i - 10000;
+            dict[c].count = hash[i];
+            c++;
+        }
+    }
+    
+    qsort(dict,c,sizeof(Dict),CompareDict);
+    
+    int * res = (int *) malloc(sizeof(int)*k);
+    c = 0;
+    *returnSize = k;
+    
+    for(int i =0; i <k;i++)
+    {
+        res[c++] = dict[i].num;
+    }
+    
+    return res;
+}
+
 int main() {
     int arr[6] = {-1, -1};
     int returnSize;
-    int* res = topKFrequent(arr, 2, 1, &returnSize);
+    int* res = topKFrequentSample(arr, 2, 1, &returnSize);
     for (int i = 0; i < returnSize; i++) {
         printf("%d ", res[i]);
     }
